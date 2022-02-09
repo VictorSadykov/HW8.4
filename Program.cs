d7a2e40b17250105e34fc8bca94afb444b3813a7
Vitya Sadykov <vityasadykov@gmail.com>
2022-02-09 23:32:19 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FinalTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"C:\test\Students.dat";
+            string outputPath = @"C:\test\Students";
+            BinaryFormatter formatter = new BinaryFormatter();
+            DirectoryInfo di = new DirectoryInfo(outputPath);
+
+            di.Create();
+            Student[] students;
+
+            if (File.Exists(inputPath))
+            {
+                using (var fs = new FileStream(inputPath, FileMode.OpenOrCreate))
+                {
+                    students = (Student[])formatter.Deserialize(fs);
+
+                    List<string> groups = new List<string>(); // Создаем список групп
+                    foreach (var student in students)
+                    {
+                        if (!groups.Contains(student.Group))
+                        {
+                            groups.Add(student.Group);
+                        }
+                    }
+
+                    foreach (var group in groups)
+                    {
+                        using (StreamWriter sw = new StreamWriter(@$"{outputPath}\{group}.txt"))
+                        {
+                            foreach (var student in students)
+                            {
+                                if (group == student.Group)
+                                {
+                                    sw.WriteLine($"Имя: {student.Name}; Дата рождения: {student.DateOfBirth}");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    [Serializable]
+    class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
